91ce026b38599805675dbe3747bb9f346c391046
Siddhi Chavan <siddhichavan073@gmail.com>
2025-07-10 10:51:07 +0530

    Add files via upload

---
+++ Maximum.java
@@ -0,0 +1,21 @@
+
+public class Maximum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =10;
+		int b = 20;
+		int c = 30;
+		
+		if(a > b && a >c) {
+			System.out.println(a + " is a maximum number.");
+		}
+		else if(b > a && b >c) {
+			System.out.println(b + " is a maximum number.");
+		}
+		else {
+			System.out.println(c + " is a maximum number.");
+		}
+	}
+
+}
---
+++ Right_angle.java
@@ -0,0 +1,16 @@
+
+public class Right_angle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 7;
+		
+		for(int i =1; i <= n; i++ ) {
+			for(int j = 1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println(); 
+		}
+	}
+
+}
