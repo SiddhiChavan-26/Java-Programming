b9ad4e631a33e876643af4c9719816d06fe5605f
Siddhi Chavan <siddhichavan073@gmail.com>
2025-07-22 20:02:22 +0530

    Add files via upload

---
+++ Label_textField/Program.java
@@ -0,0 +1,12 @@
+//Label and TextField
+package college.dkte.p3;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		Window window = new Window();
+		window.setSize(900,600);
+		window.setVisible(true);
+	}
+	
+}
