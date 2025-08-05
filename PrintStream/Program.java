5f9410b4664bbd7b2a169e2983cc4a0dd688a3e6
Siddhi Chavan <siddhichavan073@gmail.com>
2025-08-05 19:52:02 +0530

    Add files via upload

---
+++ PrintStream/Program.java
@@ -0,0 +1,32 @@
+package college.demo4;
+
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Program {
+
+		public static void main(String[] args) {
+			
+			List<Movie> list = new ArrayList<Movie>();
+			list.add(new Movie(1, "Star Wars", 7.5));
+			list.add(new Movie(2, "Godfather", 8.0));
+			list.add(new Movie(3, "Hidden Figures", 7.0));
+			list.add(new Movie(4, "Bruce Almighty", 6.5));
+			list.add(new Movie(5, "Forest Gump", 8.5));
+			
+			try(FileOutputStream fout = new FileOutputStream("Movies.db")){
+				try(PrintStream out = new PrintStream(fout)){
+					for(Movie m : list) {
+						out.printf("%-20d%-15s%.2f\n", m.getId(), m.getTitle(), m.getRating());
+					}
+				}//out.close()
+				System.out.println("Movies saved !!!");
+			}//fout.close()
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+}
