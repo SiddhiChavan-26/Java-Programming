a2bbcdbfed458ce0db9a74f7f03279a0768ad816
Siddhi Chavan <siddhichavan073@gmail.com>
2025-08-14 20:18:12 +0530

    Add files via upload

---
+++ fileCreateAndDelete/Demo7.java
@@ -0,0 +1,39 @@
+package college;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Demo7 {
+
+	public static void main1(String[] args) {
+		Scanner  sc  = new Scanner(System.in);
+		System.out.print("Enter the  source path: ");
+		String srcPath = sc.next();
+		
+		File file = new File(srcPath);
+		boolean success = file.mkdir();
+		System.out.println("Directory created: "+ success);
+
+	}
+	
+	public static void main2(String[]  args) throws IOException{
+		Scanner  sc  = new Scanner(System.in);
+		System.out.print("Enter the  source path: ");
+		String srcPath = sc.next();
+		
+		File file = new File(srcPath);
+		boolean success = file.createNewFile();
+		System.out.println("File created: "+success);
+		
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		System.out.print("Enter th source Path: ");
+		String srcPath = sc.next();
+		
+		File file = new File(srcPath);
+		boolean success = file.delete();
+		System.out.println("File deleted : "+success);
+		}
+}
