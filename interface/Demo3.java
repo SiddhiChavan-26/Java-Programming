714985643fd801fcd348e4fdf3b1d024fff67598
Siddhi Chavan <siddhichavan073@gmail.com>
2025-09-11 19:29:26 +0530

    Add files via upload

---
+++ interface/Demo3.java
@@ -0,0 +1,24 @@
+package college.dkte;
+
+interface Printable{
+	/*public static final*/int number = 10;
+	/*public abstarct*/void print();
+	
+}
+class Test implements Printable{
+	@Override
+	public void print() {
+		System.out.println("Number: "+Printable.number);
+	}
+}
+public class Demo3 {
+
+	public static void main(String[] args) {
+//		Test t1 = new Test();
+//		t1.print();
+		
+		Printable p = new Test();
+		p.print();
+	}
+
+}
