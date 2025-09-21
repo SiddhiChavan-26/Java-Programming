9a1cde32464ec6b9d3f5f0a315ceb81862dadd44
Siddhi Chavan <siddhichavan073@gmail.com>
2025-09-21 17:29:00 +0530

    Add files via upload

---
+++ toString/Date.java
@@ -0,0 +1,43 @@
+// overriding toString method of object class
+package college.dkte.demo8;
+
+public class Date {
+	private int day;
+	private int month;
+	private int year;
+	
+	public Date() {
+		//TODO
+	}
+	public Date(int day, int month, int year) {
+		this.day = day;
+		this.month = month;
+		this.year = year;
+	}
+	
+	//getters and setters
+	public void setDay(int day) {
+		this.day = day;
+	}
+	public int getDay() {
+		return day;
+	}
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	public int getMonth() {
+		return month;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getYear() {
+		return year;
+	}
+	
+	@Override
+	public String toString() {
+	    return "Date [ Day = " + day + ", Month = " + month + ", Year = " + year + " ]";
+	}
+
+}
---
+++ toString/Program.java
@@ -0,0 +1,12 @@
+//toString method
+package college.dkte.demo8;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Date dt1 = new Date(26,5,2005);
+		System.out.println("toString: "+ dt1.toString());
+
+	}
+
+}
